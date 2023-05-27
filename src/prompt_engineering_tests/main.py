e0b584da1fda9a04ffb549b4d269caac87c0f1ee
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-05-26 17:18:04 -0700

    working example

--- src/prompt_engineering_tests/main.py
+++ src/prompt_engineering_tests/main.py
@@ -1,14 +1,35 @@
 import openai
 import os
 
-
 APIKEY_SUFFIX = "apikey.txt"
 APIKEY_ENV_SUFFIX = "API_KEY"
 
 
 def demo():
-    api_key = os.environ["OPENAPI_API_KEY"]
+    api_key = os.environ["OPENAI_API_KEY"]
     openai.api_key = api_key
+
+    text = """
+        You should express what you want a model to do by \ 
+        providing instructions that are as clear and \ 
+        specific as you can possibly make them. \ 
+        This will guide the model towards the desired output, \ 
+        and reduce the chances of receiving irrelevant \ 
+        or incorrect responses. Don't confuse writing a \ 
+        clear prompt with writing a short prompt. \ 
+        In many cases, longer prompts provide more clarity \ 
+        and context for the model, which can lead to \ 
+        more detailed and relevant outputs.
+    """
+
+    prompt = f"""
+            Summarize the text delimited by triple backticks \ 
+            into a single sentence.
+            ```{text}```
+            """
+
+    response = get_completion(prompt)
+    print(response)
 
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
